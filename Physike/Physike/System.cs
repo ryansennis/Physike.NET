7d52c60567ae7240a8c97cceb46e792578ba5190
Ryan Ennis <ryansennis@hotmail.com>
2020-04-07 12:46:38 -0600

    Added System class

---
+++ Physike/Physike/System.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Util = System.Math;
+using Physike.Math;
+
+namespace Physike
+{
+    public class System
+    {
+        public List<Particle> Particles { get; }
+
+    }
+}
